e8d6b589a2353e3c4cfc802552962c68b533412c
elifala <60571627+elifala@users.noreply.github.com>
2020-02-02 16:19:01 +0200

    Update test.java

--- DenemeProjesi/src/test.java
+++ DenemeProjesi/src/test.java
@@ -9,14 +9,5 @@
  * @author win7
  */
 public class test {
-<<<<<<< Updated upstream:DenemeProjesi/src/test.java
-    public static void main (String[]args){
-        int aba = 5;
-        int bab = 4;
-=======
- 
-  
->>>>>>> Stashed changes:DenemeProjesi/test.java
-        
-        
-}}
+
+}
